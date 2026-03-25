04c09f439109802487d5a004738bc72945cf89dc
Neil Thapa <thapaa.neill@gmail.com>
2026-03-25 03:16:28 +0000

    feat: Add initial grid layout
    - Added container div
    - Implemented flexbox to make divs appear like a grid

---
+++ script.js
@@ -0,0 +1,23 @@
+const NUMBER_OF_BOXES = 256;
+
+const containerDiv = document.querySelector("#container");
+
+for (let i = 0; i < NUMBER_OF_BOXES; i++) {
+    const divSqr = document.createElement("div");
+    divSqr.setAttribute("id", "square-divs");
+    containerDiv.appendChild(divSqr);
+}
+
+const sqrDiv = document.querySelectorAll("#square-divs");
+
+sqrDiv.forEach(square => {
+    square.addEventListener("mouseenter", function(event) {
+    console.log("Mouse is inside the div!");
+});
+});
+
+sqrDiv.forEach(square => {
+    square.addEventListener("mouseleave", function(event) {
+        console.log("Mouse left the div!");
+    });
+});
