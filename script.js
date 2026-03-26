38b75983e0d2616662e1c459c095d3761ddb3c56
Neil Thapa <thapaa.neill@gmail.com>
2026-03-26 02:16:17 +0000

    refactor: reorganize DOM references and grid logic
    - Moved DOM references to the top of file
    - Moved grid-drawing logic into a function

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-const NUMBER_OF_BOXES = 256;
-
 const containerDiv = document.querySelector("#container");
-
-for (let i = 0; i < NUMBER_OF_BOXES; i++) {
-    const divSqr = document.createElement("div");
-    divSqr.classList.add("square-divs");
-    containerDiv.appendChild(divSqr);
-}
-
 const sqrDiv = document.querySelectorAll(".square-divs");
 const startBtn = document.querySelector("#start");
 const eraser = document.querySelector("#erase");
+
+drawGrid(12);
 
 let mode = "erase";
 
@@ -27,3 +20,11 @@ sqrDiv.forEach(square => {
 
 startBtn.addEventListener("click", () => mode = "draw");
 eraser.addEventListener("click",() => mode = "erase");
+
+function drawGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const divSqr = document.createElement("div");
+        divSqr.classList.add("square-divs");
+        containerDiv.appendChild(divSqr);
+    }
+}
