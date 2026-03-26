0098c0e4dea62ed15de11a11e9400b70e57872f0
Neil Thapa <thapaa.neill@gmail.com>
2026-03-26 02:41:07 +0000

    refactor: attach mouse event listeners inside grid function
    - Moved mouseenter listener to run inside the grid-drawing function

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 const containerDiv = document.querySelector("#container");
-const sqrDiv = document.querySelectorAll(".square-divs");
 const startBtn = document.querySelector("#start");
 const eraser = document.querySelector("#erase");
 
 drawGrid(12);
+const sqrDiv = document.querySelectorAll(".square-divs"); // select the divs for grid
 
 let mode = "erase";
-
-sqrDiv.forEach(square => {
-    square.addEventListener("mouseenter", function(event) {
-        if (mode === "draw") {
-            event.target.style.backgroundColor = "black";
-        }
-        else if (mode === "erase") {
-            event.target.style.backgroundColor = "white";
-        }
-    });
-});
 
 startBtn.addEventListener("click", () => mode = "draw");
 eraser.addEventListener("click",() => mode = "erase");
@@ -25,6 +14,16 @@ function drawGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const divSqr = document.createElement("div");
         divSqr.classList.add("square-divs");
+
+        divSqr.addEventListener("mouseenter", function(event) {
+            if (mode === "draw") {
+                event.target.style.backgroundColor = "black";
+            }
+            else if (mode === "erase") {
+                event.target.style.backgroundColor = "white";
+            }
+        })
+
         containerDiv.appendChild(divSqr);
     }
 }
